a3cd550efb09687d7434b8495d8dc37f19c5f756
jfbeats <71524159+jfbeats@users.noreply.github.com>
2021-07-07 12:54:59 +0000

    Deploying to gh-pages from @ jfbeats/ArweaveWebWallet@14b5121f1915a2dd0df356c46f2df73b901ef682 🚀

---
+++ precache-manifest.fca932cc1761ef228339bbbbdec48daa.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ebd9d9972e0333897eaf180e46473f9",
+    "url": "/arweaveLogo.svg"
+  },
+  {
+    "revision": "3fcbdc7347fb38df416cd8778df01854",
+    "url": "/arweaveView.svg"
+  },
+  {
+    "revision": "b074a3e0b6e7e3879104451fea3b7b75",
+    "url": "/arweaveViewLandscape.png"
+  },
+  {
+    "revision": "67669ec2f405e0b0ba77dcf081eacead",
+    "url": "/arweaveViewLandscape.svg"
+  },
+  {
+    "revision": "c7d822443c38ffd037cb",
+    "url": "/css/app.bc3fd0dc.css"
+  },
+  {
+    "revision": "a29ceb2b7a0a23b16ff0",
+    "url": "/css/chunk-0bce9150.16b4f693.css"
+  },
+  {
+    "revision": "0e7033391f3190274502",
+    "url": "/css/chunk-211cab62.c193914a.css"
+  },
+  {
+    "revision": "e37f825e8f77c2665d4b",
+    "url": "/css/chunk-581e0f74.ef1af092.css"
+  },
+  {
+    "revision": "2f3e19c0a37fd052b0bb",
+    "url": "/css/chunk-63555912.95a16442.css"
+  },
+  {
+    "revision": "4fd6096b1399ffbb1967",
+    "url": "/css/chunk-6757661e.ba857c75.css"
+  },
+  {
+    "revision": "d7eb1418eafa36000ebfb5bbffe1d790",
+    "url": "/img/add_box.d7eb1418.svg"
+  },
+  {
+    "revision": "8ebd9d9972e0333897eaf180e46473f9",
+    "url": "/img/arweave.8ebd9d99.svg"
+  },
+  {
+    "revision": "03d8665dc82ebc576abb64d3b6e876b2",
+    "url": "/img/background.03d8665d.svg"
+  },
+  {
+    "revision": "652ec40812500eeca5371c1d5ccb8201",
+    "url": "/img/cloud.652ec408.svg"
+  },
+  {
+    "revision": "544d3c922bb852a31751964c6566a9e1",
+    "url": "/img/cloud_circle.544d3c92.svg"
+  },
+  {
+    "revision": "acdf26c5a17035a4ac5afdec86502ca6",
+    "url": "/img/drop.acdf26c5.svg"
+  },
+  {
+    "revision": "068cd311e07b482fddb051d1c61cf8f0",
+    "url": "/img/ledger.068cd311.svg"
+  },
+  {
+    "revision": "1b2f7a118a9887442c4db15f9fecd9b7",
+    "url": "/img/north_east.1b2f7a11.svg"
+  },
+  {
+    "revision": "7e6518eca5779bd5bddd8b74fed60c80",
+    "url": "/img/person.7e6518ec.svg"
+  },
+  {
+    "revision": "96ab7675148df8c48fe31a46eda69cd2",
+    "url": "/img/settings.96ab7675.svg"
+  },
+  {
+    "revision": "4c056fd59a93575ae1fe1bb3a8cc8633",
+    "url": "/img/swap.4c056fd5.svg"
+  },
+  {
+    "revision": "533c66437fadadfef8860e7e43b76418",
+    "url": "/img/usd.533c6643.svg"
+  },
+  {
+    "revision": "9384eb1b118b229fc1cd379380921e45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7d822443c38ffd037cb",
+    "url": "/js/app.67cb4351.js"
+  },
+  {
+    "revision": "a29ceb2b7a0a23b16ff0",
+    "url": "/js/chunk-0bce9150.2b63e631.js"
+  },
+  {
+    "revision": "0e7033391f3190274502",
+    "url": "/js/chunk-211cab62.c2d2bb95.js"
+  },
+  {
+    "revision": "39b4da952d47efbf65d7",
+    "url": "/js/chunk-2d2311c8.eafb2723.js"
+  },
+  {
+    "revision": "e37f825e8f77c2665d4b",
+    "url": "/js/chunk-581e0f74.343bc5db.js"
+  },
+  {
+    "revision": "2f3e19c0a37fd052b0bb",
+    "url": "/js/chunk-63555912.5eacaf48.js"
+  },
+  {
+    "revision": "4fd6096b1399ffbb1967",
+    "url": "/js/chunk-6757661e.07d0a4c3.js"
+  },
+  {
+    "revision": "c0f451946c37c5c4aa11",
+    "url": "/js/chunk-vendors.9619cd98.js"
+  },
+  {
+    "revision": "6b68ae03c78da239a56f93b9d1b882a1",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,3 @@
+importScripts("/precache-manifest.fca932cc1761ef228339bbbbdec48daa.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+
