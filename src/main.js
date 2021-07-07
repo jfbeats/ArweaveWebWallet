14b5121f1915a2dd0df356c46f2df73b901ef682
JF <simplyhiphopmusic@gmail.com>
2021-07-07 08:53:47 -0400

    -

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import './registerServiceWorker'
 import { plugin as Slicksort } from 'vue-slicksort'
 import '@/assets/animations.css'
 import '@/assets/style.css'
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,30 @@
 module.exports = {
-	publicPath: process.env.GITHUB_ACTIONS ? '/' : '/'
+	publicPath: process.env.GITHUB_ACTIONS ? '/' : '/',
+
+	pwa: {
+		name: 'Arweave Wallet',
+		themeColor: '#151515',
+		manifestPath: 'manifest.json',
+		manifestOptions: {
+			icons: [
+				{
+					src: "./arweaveLogo.svg",
+					purpose: "maskable any",
+					type: "image/svg+xml",
+					sizes: "any",
+				}
+			]
+		},
+		iconPaths: {
+			favicon32: null,
+			favicon16: null,
+			appleTouchIcon: null,
+			maskIcon: null,
+			msTileImage: null,
+		},
+		workboxPluginMode: 'InjectManifest',
+		workboxOptions: {
+			swSrc: 'src/service-worker.js',
+		}
+	}
 }
