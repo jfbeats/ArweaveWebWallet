badb69820b61fcaa88a97d7daceb70be4964ed82
jfbeats <71524159+jfbeats@users.noreply.github.com>
2021-07-11 20:17:42 +0000

    Deploying to gh-pages from @ jfbeats/ArweaveWebWallet@066dfeb1372732584c78e649c31607b603ef885e 🚀

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.27be78074c5a629276a3f430b9e8b610.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.65feba66349e378c791733a895bfd5ca.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 self.__precacheManifest = [].concat(self.__precacheManifest || [])
 workbox.setConfig({ debug: true })
