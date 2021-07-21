92ac88c0419957ce39013008ff05bdaf1177689d
JF <simplyhiphopmusic@gmail.com>
2021-07-21 16:35:13 -0400

    -

--- src/components/atomic/Icon.vue
+++ src/components/atomic/Icon.vue
@@ -2,7 +2,10 @@
 	<div class="icon">
 		<transition name="slide-up" mode="out-in">
 			<div v-if="icon" :key="icon" class="icon-background">
-				<div v-if="icon == 'loader'" class="loader" />
+				<svg v-if="icon == 'loader'" class="loader" height="100" width="100" viewBox="0 0 100 100" :class="{ spin: progress == null }">
+					<circle stroke="#ffffff33" :stroke-width="thickness" fill="transparent" :r="normalizedRadius" :cx="50" :cy="50" />
+					<circle stroke="white" :stroke-dasharray="circumference + ' ' + circumference" :style="{ strokeDashoffset }" :stroke-width="thickness" fill="transparent" :r="normalizedRadius" :cx="50" :cy="50" @animationiteration="finishAnimation = false"  :class="{ spin: progress == null || finishAnimation }" />
+				</svg>
 				<span v-else-if="isSymbol" class="symbol no-select">{{ icon }}</span>
 				<img v-else class="img no-select" :src="icon" draggable="false">
 			</div>
@@ -13,13 +16,21 @@
 
 
 <script>
-import { computed } from 'vue'
+import { computed, ref, watch } from 'vue'
 export default {
-	props: ['icon'],
+	props: ['icon', 'progress'],
 	setup (props) {
 		const isSymbol = computed(() => typeof props.icon === 'string' && props.icon.length <= 2)
-		return { isSymbol }
-	}
+		const thickness = 10
+		const normalizedRadius = 50 - thickness / 2
+		const circumference = normalizedRadius * 2 * Math.PI
+		const strokeDashoffset = computed(() => circumference - (props.progress || 25) / 100 * circumference)
+		const finishAnimation = ref(false)
+		watch(() => props.progress, (value, oldValue) => {
+			if (value != null && oldValue == null) { finishAnimation.value = true }
+		})
+		return { isSymbol, thickness, normalizedRadius, circumference, strokeDashoffset, finishAnimation }
+	},
 }
 </script>
 
@@ -57,17 +68,18 @@ export default {
 	font-size: 1em;
 }
 
-.loader,
-.loader:after {
-	border-radius: 50%;
-	width: 100%;
-	height: 100%;
+.loader {
+	transform: rotate(-90deg);
+	transition: transform 0.5s ease;
 }
 
-.loader {
-	border: 0.1em solid #ffffff33;
-	border-top: 0.1em solid currentColor;
+.loader.spin {
+	transform: rotate(-135deg);
+}
+
+circle.spin {
 	animation: loader-animation 2s infinite linear;
+	animation-timing-function: cubic-bezier(0.6, 0.4, 0.4, 0.6);
 }
 
 @keyframes loader-animation {
@@ -77,5 +89,15 @@ export default {
 	100% {
 		transform: rotate(360deg);
 	}
+}
+
+svg {
+	width: 100%;
+	height: 100%;
+}
+
+circle {
+	transition: stroke-dashoffset 0.5s ease;
+	transform-origin: 50% 50%;
 }
 </style>
