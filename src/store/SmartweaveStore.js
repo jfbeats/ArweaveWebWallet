10453f183bf08b1d9a5774f4d8c7126a33a4d90e
JF <simplyhiphopmusic@gmail.com>
2021-08-01 19:13:27 -0400

    -

--- src/store/SmartweaveStore.js
+++ src/store/SmartweaveStore.js
@@ -1,5 +1,5 @@
 import { arweave } from '@/store/ArweaveStore'
-import { readContract } from 'smartweave'
+import { smartweave, readContract } from 'smartweave'
 import { reactive } from 'vue'
 
 const SmartweaveStore = reactive({
@@ -7,11 +7,16 @@ const SmartweaveStore = reactive({
 })
 
 
+
 export async function getContract (id) {
 	SmartweaveStore.contracts[id] = await readContract(arweave, id)
 	console.log(SmartweaveStore.contracts)
 }
 
-getContract('5hpzk2vzh-QiyvYqnVuaUFWr8--HfhcfK6HIix-ldJQ')
+// Testing
+if (process.env.NODE_ENV === 'development') {
+	window.smartweave = smartweave
+	getContract('5hpzk2vzh-QiyvYqnVuaUFWr8--HfhcfK6HIix-ldJQ')
+}
 
 export default SmartweaveStore
