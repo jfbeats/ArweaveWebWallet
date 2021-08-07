7337ad54bf17f15cefcf0ad7122115bb271a2f19
jfbeats <71524159+jfbeats@users.noreply.github.com>
2021-08-07 17:52:26 +0000

    Deploying to gh-pages from @ jfbeats/ArweaveWebWallet@ab6f21fe5b46db43472e721bc04364a99e63338c 🚀

---
+++ precache-manifest.3ef0ede879ecc7887d4846f245e2e496.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "728bff6123ed447a1259944bb31bb24d",
+    "url": "/arweave-192.png"
+  },
+  {
+    "revision": "c8d0678a51c5f220e6664ebf2c2e1222",
+    "url": "/arweave-512.png"
+  },
+  {
+    "revision": "cbca2b1a1c623b628f07465d17215be3",
+    "url": "/arweave.svg"
+  },
+  {
+    "revision": "9f9c498b814fb8f86b47101021a0528d",
+    "url": "/arweaveView.svg"
+  },
+  {
+    "revision": "1b743a292bd83b9511b79ced66a0543e",
+    "url": "/arweaveViewLandscape.png"
+  },
+  {
+    "revision": "078a9a9ad11a96418da3",
+    "url": "/css/app.cfe2dde6.css"
+  },
+  {
+    "revision": "99d885010a406a41b178",
+    "url": "/css/chunk-098aae8a.4121b131.css"
+  },
+  {
+    "revision": "d8e2d48ae534c3b3757b",
+    "url": "/css/chunk-0ee5eb3e.b6d1bbeb.css"
+  },
+  {
+    "revision": "d40c5dacac4dab8ae43b",
+    "url": "/css/chunk-3720c733.73f0c0a9.css"
+  },
+  {
+    "revision": "b89ddb43660feb6f9c4d",
+    "url": "/css/chunk-4970192f.f38890a4.css"
+  },
+  {
+    "revision": "4b24cf41367d01948a96",
+    "url": "/css/chunk-7cabffa4.85838ed9.css"
+  },
+  {
+    "revision": "a0fbb81919f30d2117d9",
+    "url": "/css/chunk-8935c23c.031d4220.css"
+  },
+  {
+    "revision": "d7eb1418eafa36000ebfb5bbffe1d790",
+    "url": "/img/add_box.d7eb1418.svg"
+  },
+  {
+    "revision": "cbca2b1a1c623b628f07465d17215be3",
+    "url": "/img/arweave.cbca2b1a.svg"
+  },
+  {
+    "revision": "6c6d65b98f693e3ccb34918f229a7e9c",
+    "url": "/img/arweaveBlack.6c6d65b9.svg"
+  },
+  {
+    "revision": "03d8665dc82ebc576abb64d3b6e876b2",
+    "url": "/img/background.03d8665d.svg"
+  },
+  {
+    "revision": "652ec40812500eeca5371c1d5ccb8201",
+    "url": "/img/cloud.652ec408.svg"
+  },
+  {
+    "revision": "acdf26c5a17035a4ac5afdec86502ca6",
+    "url": "/img/drop.acdf26c5.svg"
+  },
+  {
+    "revision": "463afe4b835d75b18aac49f1708cd4e4",
+    "url": "/img/label.463afe4b.svg"
+  },
+  {
+    "revision": "068cd311e07b482fddb051d1c61cf8f0",
+    "url": "/img/ledger.068cd311.svg"
+  },
+  {
+    "revision": "1b2f7a118a9887442c4db15f9fecd9b7",
+    "url": "/img/north_east.1b2f7a11.svg"
+  },
+  {
+    "revision": "7e6518eca5779bd5bddd8b74fed60c80",
+    "url": "/img/person.7e6518ec.svg"
+  },
+  {
+    "revision": "96ab7675148df8c48fe31a46eda69cd2",
+    "url": "/img/settings.96ab7675.svg"
+  },
+  {
+    "revision": "4c056fd59a93575ae1fe1bb3a8cc8633",
+    "url": "/img/swap.4c056fd5.svg"
+  },
+  {
+    "revision": "985473eddbb689f4bbf8d454c49e54d8",
+    "url": "/img/text.985473ed.svg"
+  },
+  {
+    "revision": "2ca824d367a602a825bda7e04d141854",
+    "url": "/img/x.2ca824d3.svg"
+  },
+  {
+    "revision": "1b8d47f67e3b2da483b325ca29ae00aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "078a9a9ad11a96418da3",
+    "url": "/js/app.2e32a579.js"
+  },
+  {
+    "revision": "99d885010a406a41b178",
+    "url": "/js/chunk-098aae8a.dc6623f9.js"
+  },
+  {
+    "revision": "d8e2d48ae534c3b3757b",
+    "url": "/js/chunk-0ee5eb3e.e10afa0e.js"
+  },
+  {
+    "revision": "d40c5dacac4dab8ae43b",
+    "url": "/js/chunk-3720c733.88e18346.js"
+  },
+  {
+    "revision": "b89ddb43660feb6f9c4d",
+    "url": "/js/chunk-4970192f.df336cf3.js"
+  },
+  {
+    "revision": "4b24cf41367d01948a96",
+    "url": "/js/chunk-7cabffa4.9eeede98.js"
+  },
+  {
+    "revision": "a0fbb81919f30d2117d9",
+    "url": "/js/chunk-8935c23c.aea27b3e.js"
+  },
+  {
+    "revision": "891e7366b71bb8c01189",
+    "url": "/js/chunk-vendors.3c0ea71e.js"
+  },
+  {
+    "revision": "192777a894015425309f9fb983f435da",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.67ba6c9595863a1bded2adf1e0245ed6.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.3ef0ede879ecc7887d4846f245e2e496.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 self.__precacheManifest = [].concat(self.__precacheManifest || [])
 workbox.setConfig({ debug: false })
