f071f2745c36b254c574f972884179ce0855fd9a
JF <simplyhiphopmusic@gmail.com>
2021-10-08 19:11:55 -0400

    Buffer polyfill

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import inject from '@rollup/plugin-inject'
 
 export default defineConfig({
 	plugins: [vue()],
 	resolve: {
-		alias: { 
-			'@': path.resolve(__dirname, 'src') 
+		alias: {
+			'@': path.resolve(__dirname, 'src'),
 		}
-	}
+	},
+	build: {
+		rollupOptions: {
+			plugins: [
+				inject({ Buffer: [ 'Buffer', 'Buffer'] })
+			],
+		},
+	},
 })
