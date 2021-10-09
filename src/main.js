604353fdfd3b32834d3330fcbb8467e035ce6771
JF <simplyhiphopmusic@gmail.com>
2021-10-09 10:27:43 -0400

    periodic sw updates

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { registerSW } from 'virtual:pwa-register'
 import { plugin as Slicksort } from 'vue-slicksort'
 import '@/assets/animations.css'
 import '@/assets/style.css'
+
+registerSW({
+	onRegistered (registration) {
+		registration && setInterval(() => registration.update(), 60 * 60 * 1000)
+	}
+})
 
 const app = createApp(App)
 app.use(router, Slicksort)
