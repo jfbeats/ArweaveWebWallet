1018765782c3a6ad5d2af1c43f10b4ed4b818e1d
jfbeats <71524159+jfbeats@users.noreply.github.com>
2021-10-10 15:46:31 +0000

    Deploying to gh-pages from @ jfbeats/ArweaveWebWallet@b3666fbc3b7487637c5616ac6f7a2b733cc90432 🚀

--- service-worker.js
+++
@@ -1,9 +0,0 @@
-importScripts("precache-manifest.f5df9f7f5e041d0d1b8fed0d7fd7d756.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
-
-self.__precacheManifest = [].concat(self.__precacheManifest || [])
-workbox.setConfig({ debug: false })
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
-
-self.addEventListener('message', (event) => {
-	if (event.data && event.data.type === 'SKIP_WAITING') { self.skipWaiting() }
-})
