cba9bd060c37f000bcef0c012afaa2c6f4fc6d3a
JF <simplyhiphopmusic@gmail.com>
2021-10-18 09:29:16 -0400

    refactor

--- src/functions/Channels.js
+++ src/functions/Channels.js
@@ -1,4 +1,4 @@
-import { reactive, watchEffect, watch, computed } from 'vue'
+import { reactive, watch, computed } from 'vue'
 
 const hash = new URLSearchParams(window.location.hash.slice(1))
 const origin = hash.get('origin')
@@ -22,22 +22,21 @@ window.addEventListener('storage', globalStorageListener)
 localStorage.setItem(stateChannel, JSON.stringify(stateInit))
 const { state, closeChannel } = getChannel(instance)
 const { states, closeChannels } = getChannels()
-const connectors = computed(() => Object.fromEntries(Object.entries(states).filter(([key, value]) => value.type === 'connector')))
-const clients = computed(() => Object.fromEntries(Object.entries(states).filter(([key, value]) => value.type === 'client')))
-export { origin, state, states, connectors, clients }
+const filterChannels = (filter) => Object.fromEntries(Object.entries(states).filter(([key, state]) =>
+	!Object.entries(filter || {}).find(([key, value]) => typeof value === 'function' ? !value(state[key]) : state[key] !== value)))
+const connectors = computed(() => filterChannels({ type: 'connector' }))
+const clients = computed(() => filterChannels({ type: 'client' }))
+
+export { origin, state, states, filterChannels, connectors, clients }
 
 
 
 function getChannels () {
 	const channels = {}
 	const states = reactive({})
-	const getInstanceNames = () => {
-		const result = []
-		for (const key in localStorage) {
-			if (key.includes(chPrefix)) { result.push(key.slice(chPrefix.length)) }
-		}
-		return result
-	}
+	const getInstanceNames = () => Object.entries(localStorage)
+		.filter(([key, value]) => key.slice(0, chPrefix.length) === chPrefix)
+		.map(([key, value]) => key.slice(chPrefix.length))
 	const instanciate = async (name) => {
 		channels[name] = null
 		if (!(await heartbeat(name))) { close(name); return null }
@@ -51,21 +50,19 @@ function getChannels () {
 		delete states[name]
 	}
 	const storageListener = () => {
+		const runningChannels = Object.keys(channels)
 		const storageChannels = getInstanceNames()
-		for (const channel in channels) {
-			if (storageChannels.includes(channel)) { continue }
-			close(channel)
-		}
-		for (const storageChannel of storageChannels) {
-			if (Object.keys(channels).includes(storageChannel) || storageChannel === instance) { continue }
-			instanciate(storageChannel)
+		for (const channel of [...runningChannels, ...storageChannels]) {
+			if (runningChannels.includes(channel) && storageChannels.includes(channel)) { continue }
+			else if (storageChannels.includes(channel) && channel !== instance) { instanciate(channel) }
+			else if (runningChannels.includes(channel)) { close(channel) }
 		}
 	}
-	window.addEventListener('storage', storageListener)
 	const closeChannels = () => {
 		window.removeEventListener('storage', storageListener)
-		for (const instance in channels) { close(instance) }
+		for (const channel in channels) { close(channel) }
 	}
+	window.addEventListener('storage', storageListener)
 	storageListener()
 	return { states, closeChannels }
 }
@@ -94,10 +91,8 @@ function getChannel (instanceName) {
 		window.removeEventListener('storage', storageListener)
 		stop()
 	}
-
 	window.addEventListener('storage', storageListener)
 	update(localStorage.getItem(stateChannel))
-
 	return { state, closeChannel }
 }
 
@@ -116,7 +111,7 @@ async function heartbeat (instanceName, timeout) {
 			resolve(result)
 		}
 		window.addEventListener('storage', heartbeatListener)
-		const cleanupTimeout = setTimeout(() => heartbeatReturn(false), 60000)
+		const cleanupTimeout = setTimeout(() => heartbeatReturn(false), Math.max(60000, timeout || 0))
 		if (timeout) { setTimeout(() => resolve(false), timeout) }
 	})
 	localStorage.setItem(fullKey, '')
@@ -125,7 +120,7 @@ async function heartbeat (instanceName, timeout) {
 
 function cleanHeartbeats () {
 	for (const key in localStorage) {
-		if (!key.includes(heartbeatPrefix)) { continue }
+		if (key.slice(0, heartbeatPrefix.length) !== heartbeatPrefix) { continue }
 		const relatedChannel = chPrefix + key.slice(heartbeatPrefix.length)
 		if (localStorage.getItem(relatedChannel)) { continue }
 		localStorage.removeItem(key)
@@ -135,22 +130,13 @@ function cleanHeartbeats () {
 export async function instanceStartPromise (filter, timeout) {
 	return new Promise(resolve => {
 		const watchStop = watch(() => states, () => {
-			const name = findInstance(filter)
+			const name = Object.keys(filterChannels(filter))[0]
 			if (!name) { return }
 			resolve(name)
 			watchStop()
 		}, { deep: true, immediate: true })
-		if (timeout) { setTimeout(() => resolve(false), timeout) }
+		if (timeout) { setTimeout(() => { resolve(false); watchStop() }, timeout) }
 	})
-}
-
-export function findInstance (filter) {
-	for (const name in states) {
-		const currentInstance = states[name]
-		if (!Object.entries(filter).find(attr => currentInstance[attr[0]] !== attr[1])) {
-			return name
-		}
-	}
 }
 
 
