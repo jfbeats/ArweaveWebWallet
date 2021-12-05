4a77dbf7dbcfca86f196fd8c1d27bd33421184eb
JF <simplyhiphopmusic@gmail.com>
2021-12-05 13:26:25 -0500

    -

--- src/components/composed/WalletInfo.vue
+++ src/components/composed/WalletInfo.vue
@@ -12,28 +12,22 @@
 	</transition>
 </template>
 
-<script>
+
+
+<script setup>
 import Address from '@/components/atomic/Address.vue'
 import Icon from '@/components/atomic/Icon.vue'
 import ProfileStore, { getArweaveId } from '@/store/ProfileStore'
 import { computed, watch } from 'vue'
-
 import LogoArweave from '@/assets/logos/arweave.svg?component'
 
-export default {
-	components: { Address, Icon },
-	props: ['wallet'],
-	setup (props) {
-		const arweaveId = computed(() => ProfileStore.arweaveId[props.wallet.key])
-		const walletInfo = computed(() => props.wallet.metadata || { icon: LogoArweave, name: 'Arweave wallet' })
-		watch(() => props.wallet.key, () => {
-			getArweaveId(props.wallet.key)
-		})
-
-		return { arweaveId, walletInfo }
-	},
-}
+const props = defineProps(['wallet'])
+const arweaveId = computed(() => ProfileStore.arweaveId[props.wallet.key])
+const walletInfo = computed(() => props.wallet.metadata || { icon: LogoArweave, name: 'Arweave wallet' })
+watch(() => props.wallet.key, () => getArweaveId(props.wallet.key))
 </script>
+
+
 
 <style scoped>
 .name {
