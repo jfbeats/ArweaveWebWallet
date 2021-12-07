63c10eaa4acd4effe2576395273b553924702378
JF <simplyhiphopmusic@gmail.com>
2021-12-07 16:38:50 -0500

    -

--- src/functions/Channels.js
+++ src/functions/Channels.js
@@ -88,7 +88,8 @@ export function getChannel (prefix, instanceName = '', init = {}) {
 	}
 	const initChannel = () => {} // todo remove
 	const set = (newState) => {
-		if (Array.isArray(state)) { state.splice(0, state.length, ...(newState || [])) }
+		if (!newState) { return }
+		if (Array.isArray(state)) { state.splice(0, state.length, ...newState) }
 		else {
 			for (const key in state) { !(key in newState) && delete state[key] }
 			Object.assign(state, newState)
