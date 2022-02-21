986dbfc7f5d5753338a1c025d02e9cd07e42c1ec
jfbeats <71524159+jfbeats@users.noreply.github.com>
2022-02-21 19:04:57 +0000

    Deploying to gh-pages from @ jfbeats/ArweaveWebWallet@984ad3bb32985a5c8c98f8593cb719d35b3f0373 🚀

---
+++ assets/Profile.517707eb.js
@@ -0,0 +1,2 @@
+import{_,F as c}from"./index.0a5654bd.js";import{P as d,o as n,y as r,x as e,a4 as l,a5 as p,a as t}from"./vendor.2dccf4db.js";const i={components:{FoldingLayout:c}},a=o=>(l("data-v-167aa33d"),o=o(),p(),o),f=a(()=>t("div",{class:"test-left"},"hello",-1)),u=a(()=>t("div",{class:"test-right"},"hello",-1));function h(o,v,m,x,g,y){const s=d("FoldingLayout");return n(),r(s,null,{left:e(()=>[f]),right:e(()=>[u]),_:1})}var $=_(i,[["render",h],["__scopeId","data-v-167aa33d"]]);export{$ as default};
+//# sourceMappingURL=Profile.517707eb.js.map
