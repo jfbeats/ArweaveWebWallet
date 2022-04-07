c384a4d7d5c5f4a78471be63d1ce912932cd6561
JF <simplyhiphopmusic@gmail.com>
2022-04-07 14:21:58 -0400

    Audio component

---
+++ src/components/handlers/Audio.vue
@@ -0,0 +1,24 @@
+<template>
+	<audio controls :src="ArweaveStore.gatewayURL + tx.id" :type="tags['Content-Type']" />
+</template>
+
+
+
+<script setup lang="ts">
+import ArweaveStore from '@/store/ArweaveStore'
+import { computed } from 'vue'
+import { unpackTags } from '@/functions/Transactions'
+
+const props = defineProps<{ tx: any }>()
+
+const tags = computed(() => unpackTags(props.tx.tags))
+</script>
+
+
+
+<style scoped>
+audio {
+	max-height: var(--current-vh);
+	width: 100%;
+}
+</style>
