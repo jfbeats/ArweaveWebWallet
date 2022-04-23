3b19ccb30899741f8229445098baad4cae5f52ab
JF <simplyhiphopmusic@gmail.com>
2022-04-23 08:56:24 -0400

    tooltip

---
+++ src/components/function/Tooltip.vue
@@ -0,0 +1,78 @@
+<template>
+	<div ref="tooltipEl">
+		<slot />
+		<div v-show="false">
+			<div ref="contentEl" :style="{ padding }">
+				<span v-if="content">{{ content }}</span>
+				<slot name="content"/>
+			</div>
+		</div>
+	</div>
+</template>
+
+
+
+<script setup lang="ts">
+import tippy, { animateFill, Props } from 'tippy.js'
+import 'tippy.js/dist/tippy.css'
+import 'tippy.js/dist/backdrop.css'
+import 'tippy.js/animations/shift-away.css'
+import { computed, onBeforeUnmount, onMounted, ref, watchEffect } from 'vue'
+
+const props = defineProps<{
+	content?: any
+	interactive?: boolean
+	placement?: Props['placement']
+}>()
+
+const settings: Partial<Props> = {
+	animateFill: true,
+	plugins: [animateFill],
+	maxWidth: 'none',
+	theme: 'glass',
+}
+
+const tooltipEl = ref(null as null | HTMLElement)
+const contentEl = ref(null as null | HTMLElement)
+let tooltip: ReturnType<typeof tippy>[0]
+
+const setProps = () => tooltip.setProps({
+	...(props || []),
+	content: contentEl.value!,
+	popperOptions: { modifiers: [{ name: 'flip', options: { padding: 48 } }] },
+})
+
+onMounted(async () => {
+	const beforeUnmount = new Promise<void>(res => onBeforeUnmount(res))
+	tooltip = tippy(tooltipEl.value!, settings)
+	watchEffect(setProps)
+	await beforeUnmount
+	tooltip.destroy()
+})
+
+const padding = computed(() => props.content ? 'var(--spacing)' : '0')
+</script>
+
+
+
+<style scoped>
+.content {
+	/*padding: v-bind(padding);*/
+}
+</style>
+
+
+
+<style>
+.tippy-box[data-theme~='glass'] {
+	backdrop-filter: blur(16px);
+}
+
+.tippy-box[data-theme~='glass'] > .tippy-backdrop {
+	background-color: #08080866;
+}
+
+.tippy-box[data-theme~='glass'] > .tippy-content {
+	padding: 0;
+}
+</style>
