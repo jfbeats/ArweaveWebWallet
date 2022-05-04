f5bdd8a3506d069aaa156f35bbff945630806c6c
JF <simplyhiphopmusic@gmail.com>
2022-05-04 11:42:03 -0400

    Tooltip

--- src/components/atomic/Address.vue
+++ src/components/atomic/Address.vue
@@ -1,19 +1,28 @@
 <template>
-	<div class="address ellipsis">
-		<span class="address-tx ellipsis" @click="tools = !tools">
-			<slot />
-			<span class="text ellipsis">{{ val }}</span>
-			<Icon :icon="IconVerify" v-if="arverify?.verified" class="arverify" />
-		</span>
-	</div>
+	<Tooltip disabled>
+		<template #content>
+			<div class="text">
+				{{ address }}
+			</div>
+		</template>
+		<div class="address ellipsis">
+			<span class="address-tx ellipsis" @click="tools = !tools">
+				<slot />
+				<span class="text ellipsis">{{ val }}</span>
+				<Icon :icon="IconVerify" v-if="arverify?.verified" class="arverify" />
+			</span>
+		</div>
+	</Tooltip>
 </template>
 
 
 
 <script setup lang="ts">
 import Icon from '@/components/atomic/Icon.vue'
+import Tooltip from '@/components/function/Tooltip.vue'
 import ProfileStore, { getArverify } from '@/store/ProfileStore'
 import { computed, watch, ref } from 'vue'
+
 import IconVerify from '@/assets/icons/verify.svg?component'
 
 const props = defineProps<{
@@ -34,11 +43,13 @@ const tools = ref(false)
 <style scoped>
 .address {
 	display: inline-flex;
+	max-width: 100%;
 }
 
 .address-tx {
 	display: inline-flex;
 	align-items: center;
+	white-space: nowrap;
 }
 
 .text {
@@ -46,6 +57,7 @@ const tools = ref(false)
 	font-family: monospace, monospace;
 	font-size: 1em;
 	user-select: all;
+	white-space: nowrap;
 }
 
 .arverify {
