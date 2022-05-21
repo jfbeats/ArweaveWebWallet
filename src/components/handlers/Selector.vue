696a8d7d1263aba38632174d08649473d4f0bf0d
JF <simplyhiphopmusic@gmail.com>
2022-05-21 18:13:09 -0400

    data query fallback + refactor

--- src/components/handlers/Selector.vue
+++ src/components/handlers/Selector.vue
@@ -1,29 +1,30 @@
 <template>
-	<div v-if="data.handler === 'intent'" @click="data.intent = true" class="selector data-container min-height box" style="display: flex; justify-content: center; padding-top: 5em;">
-		<Button :icon="IconDownload">Load large file</Button>
-	</div>
-	<div v-else-if="data.handler === 'raw'" key="json" class="selector data-container min-height box">
-		<TransitionsManager>
-			<LoaderBlock v-if="!data.loaded" class="loader" />
-		</TransitionsManager>
-		<pre class="raw">{{ data.payload }}</pre>
-	</div>
-	<div v-else-if="data.handler" class="selector">
-		<TransitionsManager>
-			<LoaderBlock v-if="!data.loaded" class="loader" />
-		</TransitionsManager>
-		<TransitionsManager>
-			<div v-show="data.loaded" v-bind="data.handler.containerAttrs" class="box">
-				<component :is="data.handler.is" v-bind="data.handler.attrs" @load="data.loaded = true" />
-			</div>
-		</TransitionsManager>
-	</div>
+	<TransitionsManager>
+		<div v-if="data.handler === 'intent'" @click="data.intent = true" class="selector data-container min-height box" style="display: flex; justify-content: center; padding-top: 5em;">
+			<Button :icon="IconDownload">Load large file</Button>
+		</div>
+		<div v-else-if="data.handler === 'raw'" key="json" class="selector data-container min-height box">
+			<TransitionsManager>
+				<LoaderBlock v-if="!data.loaded" class="loader" />
+				<pre v-else class="raw">{{ data.payload }}</pre>
+			</TransitionsManager>
+		</div>
+		<div v-else-if="data.handler" class="selector">
+			<TransitionsManager>
+				<LoaderBlock v-if="!data.loaded" class="loader" />
+			</TransitionsManager>
+			<TransitionsManager>
+				<div v-show="data.loaded" v-bind="data.handler.containerAttrs" class="box">
+					<component :is="data.handler.is" v-bind="data.handler.attrs" @load="data.loaded = true" />
+				</div>
+			</TransitionsManager>
+		</div>
+	</TransitionsManager>
 </template>
 
 
 
 <script setup lang="ts">
-import ArweaveStore, { arweave, arweaveQuery } from '@/store/ArweaveStore'
 import Img from '@/components/handlers/Img.vue'
 import Video from '@/components/handlers/Video.vue'
 import Button from '@/components/atomic/Button.vue'
@@ -31,9 +32,10 @@ import List from '@/components/layout/List.vue'
 import TxCard from '@/components/composed/TxCard.vue'
 import LoaderBlock from '@/components/layout/LoaderBlock.vue'
 import TransitionsManager from '@/components/visual/TransitionsManager.vue'
-import { computed, markRaw, reactive, watch } from 'vue'
-import { unpackTags } from '@/functions/Transactions'
 import IconDownload from '@/assets/icons/download.svg?component'
+import ArweaveStore, { arweave, arweaveQuery } from '@/store/ArweaveStore'
+import { unpackTags } from '@/functions/Transactions'
+import { computed, markRaw, reactive, ref, watch } from 'vue'
 
 type Handler = {
 	is: string | object
@@ -43,17 +45,28 @@ type Handler = {
 
 const props = defineProps(['tx'])
 
+const gatewayLink = computed(() => ArweaveStore.gatewayURL + props.tx.id)
+const smartweaveLink = computed(() => 'https://arcode.studio/#/' + props.tx.id + '?theme=dark-blue&hideToolbar=true')
+
 const data = reactive({
 	handler: undefined as undefined | Handler | 'intent' | 'raw',
 	payload: undefined as any,
 	loaded: false,
 	intent: false,
 })
+const rawWhiteSpace = ref(undefined as undefined | string)
 
-const gatewayLink = computed(() => ArweaveStore.gatewayURL + props.tx.id)
-const smartweaveLink = computed(() => 'https://arcode.studio/#/' + props.tx.id + '?theme=dark-blue&hideToolbar=true')
+watch(() => props.tx.id, () => {
+	data.handler = undefined
+	data.payload = undefined
+	data.loaded = false
+	data.intent = false
+	rawWhiteSpace.value = 'pre-wrap'
+	load()
+}, { immediate: true })
+watch(() => data.intent, () => data.intent && load())
 
-const load = async () => {
+async function load () {
 	if (!props.tx || props.tx.data?.size === '0' || props.tx.data?.size == null) { return }
 	const tags = unpackTags(props.tx.tags)
 	if (tags['Bundle-Version']) {
@@ -64,47 +77,32 @@ const load = async () => {
 			containerAttrs: { class: ['data-container', 'column-container', 'padding'] }
 		}
 	}
-	if (tags['Content-Type'] === 'application/x.arweave-manifest+json' || tags['Content-Type'] === 'text/html' || tags['Content-Type'] === 'application/pdf') { return data.handler = { is: 'iframe', attrs: { src: gatewayLink.value, class: ['hover'] }, containerAttrs: { class: ['iframe-container', 'fixed-height'] } } }
-	if (tags['Content-Type']?.split('/')[0] === 'video') { data.loaded = true; return data.handler = { is: markRaw(Video), attrs: { tx: props.tx }, containerAttrs: { class: ['iframe-container'] } } }
-	if (tags['Content-Type']?.split('/')[0] === 'audio') { data.loaded = true; return data.handler = { is: markRaw(Video), attrs: { tx: props.tx }, containerAttrs: { class: ['iframe-container'] } } }
-	// if (tags['Content-Type']?.split('/')[0] === 'audio') { return data.handler = { is: 'iframe', attrs: { src: gatewayLink.value }, containerAttrs: { class: ['iframe-container', 'fixed-height'] } } }
-	if (props.tx.data.size > 104857600 && !data.intent) { return data.handler = 'intent' }
-	if (tags['Content-Type']?.split('/')[0] === 'image') { return data.handler = { is: markRaw(Img), attrs: { src: gatewayLink.value }, containerAttrs: { class: ['img-container'] } } }
-	if (tags['App-Name'] === 'SmartWeaveContract' || tags['App-Name'] === 'SmartWeaveContractSource') { return data.handler = { is: 'iframe', attrs: { src: smartweaveLink.value }, containerAttrs: { class: ['iframe-container', 'fixed-height'] } } }
+	else if (tags['Content-Type'] === 'application/x.arweave-manifest+json' || tags['Content-Type'] === 'text/html' || tags['Content-Type'] === 'application/pdf') { return data.handler = { is: 'iframe', attrs: { src: gatewayLink.value, class: ['hover'] }, containerAttrs: { class: ['iframe-container', 'fixed-height'] } } }
+	else if (tags['Content-Type']?.split('/')[0] === 'video') { data.loaded = true; return data.handler = { is: markRaw(Video), attrs: { tx: props.tx }, containerAttrs: { class: ['iframe-container'] } } }
+	else if (tags['Content-Type']?.split('/')[0] === 'audio') { data.loaded = true; return data.handler = { is: markRaw(Video), attrs: { tx: props.tx }, containerAttrs: { class: ['iframe-container'] } } }
+	// else if (tags['Content-Type']?.split('/')[0] === 'audio') { return data.handler = { is: 'iframe', attrs: { src: gatewayLink.value }, containerAttrs: { class: ['iframe-container', 'fixed-height'] } } }
+	else if (props.tx.data.size > 104857600 && !data.intent) { return data.handler = 'intent' }
+	else if (tags['Content-Type']?.split('/')[0] === 'image') { return data.handler = { is: markRaw(Img), attrs: { src: gatewayLink.value }, containerAttrs: { class: ['img-container'] } } }
+	else if (tags['App-Name'] === 'SmartWeaveContract' || tags['App-Name'] === 'SmartWeaveContractSource') { return data.handler = { is: 'iframe', attrs: { src: smartweaveLink.value }, containerAttrs: { class: ['iframe-container', 'fixed-height'] } } }
 	else {
 		data.handler = 'raw'
-		try {
-			if (tags['Content-Type'] === 'application/gzip') { data.payload = await gzipDecompress(props.tx.id) }
-			else {
-				try {
-					data.payload = (await arweave.api.get(props.tx.id)).data
-					if (data.payload[0] === "{") { try { data.payload = JSON.stringify(JSON.parse(data.payload), null, 2) } catch (e) { } }
-				} catch (e) { }
-			}
-		} catch (e) { }
+		try { if (tags['Content-Type'] === 'application/gzip') { data.payload ??= await gzipDecompress(props.tx.id) } } catch (e) { console.error(e) }
+		try { data.payload ??= (await arweave.api.get(props.tx.id)).data } catch (e) { }
+		try { data.payload ??= await arweave.transactions.getData(props.tx.id, { decode: true, string: true }) } catch (e) { }
+		if (data.payload[0] === '{' || data.payload[0] === '[') { try { data.payload = JSON.stringify(JSON.parse(data.payload), null, 2); rawWhiteSpace.value = 'pre' } catch (e) { } }
+		data.loaded = true
 	}
-	data.loaded = true
 }
 
 async function gzipDecompress(tx: string) {
-	const res = (await arweave.api.get(tx, {responseType: 'blob'})).data
+	let res: any
+	try { res ??= (await arweave.api.get(tx, {responseType: 'blob'})).data } catch (e) { }
+	try { res ??= new Blob([await arweave.transactions.getData(tx, { decode: true })]) } catch (e) { }
 	const ds = new DecompressionStream('gzip')
 	const decompressedStream = res.stream().pipeThrough(ds)
 	const blob = await new Response(decompressedStream).blob()
 	return blob.text()
 }
-
-watch(() => props.tx.id, () => {
-	data.handler = undefined
-	data.payload = undefined
-	data.loaded = false
-	data.intent = false
-	load()
-}, { immediate: true })
-
-watch(() => data.intent, () => {
-	data.intent && load()
-})
 </script>
 
 
@@ -165,7 +163,7 @@ iframe.hover:hover {
 	overflow: auto;
 	padding: var(--spacing);
 	margin: 0;
-	white-space: pre-wrap;
+	white-space: v-bind(rawWhiteSpace);
 }
 
 .column-container {
