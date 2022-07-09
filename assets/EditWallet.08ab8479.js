2fb0a493a47bf2700cc9b6a307f33823a87b090d
jfbeats <71524159+jfbeats@users.noreply.github.com>
2022-07-09 17:54:31 +0000

    Deploying to gh-pages from @ jfbeats/ArweaveWebWallet@96a6c66c7dfb8cc897eefbaae5f016f0bd3fe641 🚀

--- assets/EditWallet.08ab8479.js
+++
@@ -1,2 +0,0 @@
-import{W as e}from"./WalletsOptions.b1fa6e05.js";import{_ as t,d as a,o as s,g as _,q as o}from"./index.6637ec10.js";import"./download.d9fc6ca1.js";const d={class:"edit-wallet"},r=a({setup(l){return(c,i)=>(s(),_("div",d,[o(e,{class:"card"})]))}});var m=t(r,[["__scopeId","data-v-2e620fe5"]]);export{m as default};
-//# sourceMappingURL=EditWallet.08ab8479.js.map
---
+++ assets/EditWallet.2783c78e.js
@@ -0,0 +1,2 @@
+import{W as e}from"./WalletsOptions.f870931d.js";import{_ as t,d as a,o as s,g as _,q as o}from"./index.d94c246a.js";import"./download.32bef3c6.js";const d={class:"edit-wallet"},r=a({setup(l){return(c,i)=>(s(),_("div",d,[o(e,{class:"card"})]))}});var m=t(r,[["__scopeId","data-v-2e620fe5"]]);export{m as default};
+//# sourceMappingURL=EditWallet.2783c78e.js.map
