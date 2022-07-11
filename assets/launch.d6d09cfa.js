68f8c556ffd4ff4f3d38855b4be7631f9a973529
jfbeats <71524159+jfbeats@users.noreply.github.com>
2022-07-11 16:51:19 +0000

    Deploying to gh-pages from @ jfbeats/ArweaveWebWallet@d9df81f5d0a0e0279024bb06f6bf312393359e35 🚀

---
+++ assets/launch.d6d09cfa.js
@@ -0,0 +1,2 @@
+import{o,g as t,b as e}from"./index.3cb731b4.js";const n={xmlns:"http://www.w3.org/2000/svg",viewBox:"0 0 24 24"},h=e("path",{d:"M0 0h24v24H0V0z",fill:"none"},null,-1),s=e("path",{d:"M19 19H5V5h7V3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z"},null,-1),c=[h,s];function a(r,l){return o(),t("svg",n,c)}var d={render:a};export{d as I};
+//# sourceMappingURL=launch.d6d09cfa.js.map
