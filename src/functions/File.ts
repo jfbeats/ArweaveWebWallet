43ece34c4c1fe49ebc0fa99f7702aca14e5d7a72
JF <simplyhiphopmusic@gmail.com>
2022-08-06 19:23:24 -0400

    key import fix

--- src/functions/File.ts
+++ src/functions/File.ts
@@ -17,8 +17,10 @@ export async function dropped (e?: DragEvent | InputEvent | FileSystemDirectoryH
 	if (!e) { return addFiles([]) }
 	const text = eventToText(e)
 	const files = await eventToFiles(e)
-	const keyfiles = await findKeyfiles(text ? [text] : files)
-	// if (keyfiles.length) { return importKeyfiles(keyfiles) } // todo require async isProviderFor
+	if (type === 'keyfile') { // todo require async isProviderFor to try on any dropped type
+		const keyfiles = await findKeyfiles(text ? [text] : files)
+		if (keyfiles.length) { return importKeyfiles(keyfiles) }
+	}
 	if (type === 'keyfile') { throw 'Error importing keyfiles' }
 	// todo unsigned tx - fill send page (through tx import function?)
 	// todo signed tx - ask to upload (through tx import function?)
