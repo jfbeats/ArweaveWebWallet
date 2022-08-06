07aefb4136848be105808a556ba79b7b4c1be4f5
jfbeats <jfbeats@users.noreply.github.com>
2022-08-06 23:24:57 +0000

    Deploying to gh-pages from @ jfbeats/ArweaveWebWallet@955d0caa573f887c5bb9ddd0197a3a7edda40e61 🚀

--- assets/EditWallet.efb03754.js
+++
@@ -1,2 +0,0 @@
-import{W as e}from"./WalletsOptions.8cd2b2f7.js";import{_ as t,d as a,o as s,g as _,q as o}from"./index.cd644976.js";import"./download.eef89654.js";const d={class:"edit-wallet"},r=a({setup(l){return(c,i)=>(s(),_("div",d,[o(e,{class:"card"})]))}});var m=t(r,[["__scopeId","data-v-2e620fe5"]]);export{m as default};
-//# sourceMappingURL=EditWallet.efb03754.js.map
