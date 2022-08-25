b9d46e9cc134602b1e251fa5775dd1f850c838b3
JF <simplyhiphopmusic@gmail.com>
2022-08-25 08:49:30 -0400

    arlocal mint feature

--- src/components/composed/Balance.vue
+++ src/components/composed/Balance.vue
@@ -1,8 +1,11 @@
 <template>
 	<div class="balance flex-column">
 		<div class="amounts">
-			<div>
+			<div class="flex-row" style="align-items: start">
 				<Ar :ar="wallet.balance" />
+				<Button v-if="wallet.balance && mint" :run="mint" style="height: 2.1em; width: 2em;">
+					<Icon icon="+" />
+				</Button>
 			</div>
 			<div>
 				<LocaleCurrency :ar="wallet.balance" />
@@ -14,12 +17,24 @@
 
 
 
-<script setup>
+<script setup lang="ts">
 import Ar from '@/components/atomic/Ar.vue'
 import LocaleCurrency from '@/components/atomic/LocaleCurrency.vue'
 import WalletInfo from '@/components/composed/WalletInfo.vue'
+import Button from '@/components/atomic/Button.vue'
+import Icon from '@/components/atomic/Icon.vue'
+import ArweaveStore, { networkInfo } from '@/store/ArweaveStore'
+import { computed } from 'vue'
 
-const props = defineProps(['wallet'])
+const props = defineProps<{ wallet: Wallet }>()
+
+const mint = computed(() => {
+	if (!networkInfo.value?.network?.includes('arlocal')) { return }
+	return async () => {
+		await fetch(ArweaveStore.gatewayURL + 'mint/' + props.wallet.key + '/1000000000000')
+		props.wallet.queryBalance.getState(true)
+	}
+})
 </script>
 
 
