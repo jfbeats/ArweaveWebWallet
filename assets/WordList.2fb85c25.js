8ff4178239d164cb3fda6a020ce94b64af4fc6a5
jfbeats <jfbeats@users.noreply.github.com>
2023-02-18 23:35:29 +0000

    Deploying to gh-pages from @ jfbeats/ArweaveWebWallet@1abc2456ee5270fa15342ffc5d0aa0d5195f8cfe 🚀

--- assets/WordList.2fb85c25.js
+++
@@ -1,2 +0,0 @@
-import{_ as r,d as i,b4 as o,o as t,g as e,$ as c,Z as d,u as l,b5 as _,t as p}from"./index.7ba8413b.js";const u={class:"word-list flex-row",style:{display:"flex","justify-content":"space-between","flex-wrap":"wrap"}},f=i({setup(v){const a=o(()=>_("english"));return(g,m)=>(t(),e("div",u,[(t(!0),e(c,null,d(l(a),(s,n)=>(t(),e("div",null,p(`${n+1}. ${s}`),1))),256))]))}});var x=r(f,[["__scopeId","data-v-35c09070"]]);export{x as default};
-//# sourceMappingURL=WordList.2fb85c25.js.map
