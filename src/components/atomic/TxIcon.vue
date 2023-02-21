2e3851d41b1959007809b81a86964842aede609e
JF <simplyhiphopmusic@gmail.com>
2023-02-21 14:08:06 -0500

    tx thumbnail when image

--- src/components/atomic/TxIcon.vue
+++ src/components/atomic/TxIcon.vue
@@ -1,19 +1,23 @@
 <template>
-	<div class="tx-icon" :class="{ isPending: options.status === 'pending' }" :style="styleObject">
+	<Observer class="tx-icon" :class="{ isPending: options.status === 'pending' }" :style="styleObject" @intersection="intersection">
 		<Icon :icon="icon" class="tx-svg" />
 		<transition name="fade">
 			<Icon v-if="options.status === 'pending' || uploadProgress" icon="loader" :progress="uploadProgress" class="loader" />
 		</transition>
-	</div>
+		<transition name="fade-fast">
+			<img class="image" v-if="src && canLoad" v-show="loaded" @load="loaded = true" :src="src" alt="thumbnail" draggable="false" @dragstart.prevent />
+		</transition>
+	</Observer>
 </template>
 
 
 
 <script setup lang="ts">
 import Icon from '@/components/atomic/Icon.vue'
+import Observer from '@/components/function/Observer.vue'
 import ArweaveStore from '@/store/ArweaveStore'
 import { unpackTags } from '@/functions/Transactions'
-import { computed } from 'vue'
+import { computed, ref, watch } from 'vue'
 
 import IconTxIn from '@/assets/icons/tx_in.svg?component'
 import IconTxOut from '@/assets/icons/tx_out.svg?component'
@@ -35,9 +39,10 @@ const props = defineProps<{
 	options: TxDisplayOptions
 }>()
 
+const tags = computed(() => unpackTags(props.tx.tags, { lowercase: true }))
 const uploadProgress = computed(() => ArweaveStore.uploads[props.tx.id]?.upload)
 const icon = computed(() => {
-	if (unpackTags(props.tx.tags)['Bundle-Version']) return IconCube
+	if (tags.value['bundle-version']) return IconCube
 	if (props.options.direction === 'in' && !props.options.isData) return IconTxIn
 	if (props.options.direction === 'out' && !props.options.isData) return IconTxOut
 	if (props.options.direction === 'in' && !props.options.isValue) return IconTxInData
@@ -48,6 +53,15 @@ const icon = computed(() => {
 const styleObject = computed(() => ({
 	color: props.options.isData && !props.options.isValue ? 'var(--orange)' : props.options.direction === 'in' ? 'var(--green)' : 'var(--red)',
 }))
+const src = computed(() => !props.options.isValue && tags.value['content-type']?.startsWith('image') && (ArweaveStore.gatewayURL + props.tx.id) || undefined)
+const loaded = ref(false)
+const canLoad = ref(false)
+const intersecting = ref(false)
+const intersection = (e: IntersectionObserverEntry) => { intersecting.value = e.isIntersecting }
+watch(() => ({ src: src.value, intersecting: intersecting.value }), async (value, oldValue) => {
+	if (value.src !== oldValue?.src) { loaded.value = false }
+	if (!!value.src == value.intersecting) { canLoad.value = value.intersecting }
+}, { immediate: true })
 </script>
 
 
@@ -79,5 +93,19 @@ const styleObject = computed(() => ({
 	bottom: 0;
 	left: 0;
 	right: 0;
+}
+
+.image {
+	position: absolute;
+	width: 100%;
+	height: 100%;
+	top: 0;
+	left: 0;
+	right: 0;
+	bottom: 0;
+	object-fit: cover;
+	background: var(--background);
+	border-radius: inherit;
+	overflow: hidden;
 }
 </style>
